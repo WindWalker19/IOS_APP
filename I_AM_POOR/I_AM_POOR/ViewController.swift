28de1887475d6f0752e78b261cd1c68d20bc5e3e
Sonam sherpa <sherpasonam44@hotmail.com>
2020-09-18 23:23:36 -0700

    Add files via upload

---
+++ I_AM_POOR/I_AM_POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I_AM_POOR
+//
+//  Created by Sonam Sherpa on 9/10/20.
+//  Copyright © 2020 Sonam Sherpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
